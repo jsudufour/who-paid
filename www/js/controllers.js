4d24f3663657926b44c1732c195cbf4a295957eb
jsudufour <jsudufour@gmail.com>
2016-03-05 18:41:06 -0500

    Added form to enter new expense info, and ExpenseCtrl to add the new expenses to Firebase.

---
+++ www/js/controllers.js
@@ -0,0 +1,82 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('DashboardsCtrl', function($scope) {
+  $scope.dashboards = [
+    { title: 'Expenses per Category', id: 1 },
+    { title: 'Expenses per Month', id: 2 },
+    { title: 'Expenses per Person', id: 3 },
+    { title: 'Expenses vs Budget', id: 4 }
+  ];
+})
+
+.controller('DashboardCtrl', function($scope, $stateParams) {
+})
+
+.controller('ExpensesCtrl', function($scope, $log, $firebaseArray) {
+
+   var CONTROLLER_ID = 'ExpensesCtrl';
+
+    var vm = $scope;
+    //bindable methods
+    vm.addExpense = addExpense;
+    vm.newExpense = {};
+
+    function activate() {
+
+      $log.debug(CONTROLLER_ID + " activated");
+
+      var ref = new Firebase("https://whopaid.firebaseio.com/");
+
+      vm.expenses = $firebaseArray(ref);
+    }
+
+    activate();
+
+    function addExpense() {
+      var now = new Date();
+      vm.newExpense.date = now.toDateString();
+      vm.expenses.$add(vm.newExpense);
+      vm.newExpense = {};
+    }
+});
